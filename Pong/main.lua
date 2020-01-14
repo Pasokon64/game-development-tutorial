ffa5d5ced22a6bd67cedd42ddf1a4f089d4bdd15
Manoel Almeida <tmanoel64x@gmail.com>
2020-01-13 21:45:56 -0300

    Pong - Display frame rate

--- Pong/main.lua
+++ Pong/main.lua
@@ -27,6 +27,8 @@ PADDLE_SPEED = 200
 function love.load()
     -- Use nearest neighbor filter
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    -- Set the title in the application window
+    love.window.setTitle('Pong')
 
     math.randomseed(os.time())
 
@@ -126,6 +128,15 @@ function love.draw()
 
     ball:render()
 
+    -- DISPLAY FPS
+    displayFPS()
+
     -- End rendering in virtual resolution
     push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255/255, 0, 255/255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
